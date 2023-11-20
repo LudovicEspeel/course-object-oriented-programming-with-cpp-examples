69739c8cd0acf961e5e8619b937288d3698dec19
Ludovic Espeel <ludovic.espeel>
2023-11-20 16:04:46 +0100

    add example rgbledbar

---
+++ rgbledbar/RgbLedBar.cpp
@@ -0,0 +1,43 @@
+#include "RgbLedBar.h"
+
+namespace Hardware {
+
+  RgbLedBar::RgbLedBar(void)
+    : RgbLedBar(DEFAULT_SIZE) {
+  }
+
+  RgbLedBar::RgbLedBar(unsigned int numberOfLeds) {
+    this->numberOfLeds = numberOfLeds;
+    // Dynamically allocate memory for the array of leds
+    leds = new RgbLed[this->numberOfLeds];
+  }
+
+  RgbLedBar::~RgbLedBar(void) {
+    delete[] leds;
+    leds = nullptr;
+  }
+
+  RgbLed *RgbLedBar::getLed(unsigned int index) {
+    if (index >= numberOfLeds) {
+      return nullptr;
+    }
+
+    return &leds[index];
+  }
+
+  unsigned int RgbLedBar::getLength(void) {
+    return numberOfLeds;
+  }
+
+  std::string RgbLedBar::getString(void) {
+    std::string output = "RGB Led Bar: ";
+    for (unsigned int i = 0; i < numberOfLeds; i++) {
+      output += leds[i].getString();
+      if (i < numberOfLeds-1) {
+         output += " <=> ";
+      }
+    }
+    return output;
+  }
+
+};
